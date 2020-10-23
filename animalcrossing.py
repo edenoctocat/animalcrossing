0be51f8cfc48add5113de6a9b8ffa08af8bfcc2e
eden reinfurt <edenreinfurt@spaceputer.local>
2020-10-23 16:46:34 -0400

    initial commit

---
+++ animalcrossing.py
@@ -0,0 +1,166 @@
+import random
+import shapemodule
+
+class Island:
+	def __init__(self):
+		pass
+
+class Tree:
+	def __init__(self, type, probstick, probbell):
+		self.type = type
+		self.probstick = probstick
+		self.probbell = probbell
+		self.probnothing = 10 - (probstick + probbell)
+
+	def shake(self):
+		itemsintree = []
+		numsticks = self.probstick * 10
+		for x in range(numsticks):
+			itemsintree.append('stick')
+		numbells = self.probbell * 10
+		for x in range(numbells):
+			itemsintree.append('bell')
+		numnothing = self.probnothing * 10
+		for x in range(numnothing):
+			itemsintree.append('nothing')
+		itemfromtree = random.choice(itemsintree)
+		if itemfromtree == 'stick':
+			print('\na stick fell out of the tree')
+			stick = Stick()
+			stick.draw()
+			pickup = raw_input('do you want to pick up the stick? ')
+			if pickup == 'yes':
+				my_pocket.add('stick')
+			else: 
+				pass
+		elif itemfromtree == 'bell':
+			print('\na bell fell out of the tree')
+		else:
+			print('\nnothing fell out of the tree')
+
+	def draw(self):
+		if self.type == 'maple':
+			mycanvas = shapemodule.Canvas(40, 23)
+			trunk = shapemodule.Rectangle(9, 11, 3, 7)
+			trunk.paint(mycanvas, '| ')
+			leaves = shapemodule.Circle(5, 10, 6)
+			leaves.paint(mycanvas, 'O ')
+			mycanvas.display()
+
+		if self.type == 'cedar':
+			mycanvas = shapemodule.Canvas(40, 23)
+			box = shapemodule.Rectangle(10, 8, 8, 8)
+			box.paint(mycanvas, '* ')
+			mycanvas.display()
+
+class Stick:
+	def __init__(self):
+		pass
+
+	def draw(self):
+		mycanvas = shapemodule.Canvas(40, 22)
+		stick = shapemodule.Rectangle(6, 10, 8, 3)
+		stick.paint(mycanvas, '- ')
+		twig1 = shapemodule.Rectangle(11, 8, 1, 2)
+		twig1.paint(mycanvas, '/ ')
+		twig2 = shapemodule.Rectangle(10, 9, 1, 1)
+		twig2.paint(mycanvas, '/ ')
+		leaf = shapemodule.Circle(2, 12, 7)
+		leaf.paint(mycanvas, '0 ')
+		mycanvas.display()
+
+class BodyOfWater:
+	def __init__(self):
+		pass
+
+	def fishin(self):
+		pass
+
+class Ocean(BodyOfWater):
+	def __init__(self, fishlist):
+		self.fishlist = fishlist
+
+	def fishin(self):
+		o1 = raw_input('waiting for a fish to bite...')
+		o2 = raw_input('\na fish bit the line!')
+		fish = random.choice(self.fishlist)
+		print('\nI caught a ' + fish)
+		my_pocket.add(fish)
+
+class River(BodyOfWater):
+	def __init__(self, fishlist):
+		self.fishlist = fishlist
+
+	def fishin(self):
+		pass
+
+class Pocket:
+	def __init__(self, inventory, spaces):
+		self.inventory = inventory
+		self.spaces = spaces
+		for object in self.inventory: 
+			self.spaces -= 1
+
+	def add(self, object):
+		if self.checkspaces() == False:
+			print('\nHuh? My pockets are full.')
+		else:
+			self.inventory.append(object)
+			self.spaces -= 1
+
+	def checkspaces(self):
+		if self.spaces == 0:
+			return False
+		else:
+			return True
+
+	def takeout(self, object):
+		self.inventory.remove(object)
+		self.spaces += 1
+
+	def list(self):
+		for object in self.inventory: 
+			print(object)
+
+
+#main
+
+my_pocket = Pocket(['stone', 'sea bass'], 5)
+print('\nmy pocket contains:')
+my_pocket.list()
+print('')		
+a1 = raw_input('\nwalking towards a tree...')
+
+#this_object = raw_input('object to add to pockets: ')
+#my_pocket.add(this_object)
+#print('')		
+#print('my pocket now contains:')
+#my_pocket.list()
+#print('')
+
+this_tree = Tree('maple', 3, 1)
+this_tree.draw()
+shaketree = raw_input('do you want to shake this tree? ')
+if shaketree == 'yes':
+	this_tree.shake()
+else:
+	pass
+
+print('\nmy pocket contains:')
+my_pocket.list()
+
+a3 = raw_input('')
+
+fishing = raw_input('do you want to go fishing? ')
+if fishing == 'yes':
+	a2 = raw_input('\ngoing to the ocean...')
+	myocean = Ocean(['dace', 'sea bass', 'horse mackerel', 'red snapper', 'barred knifejaw', 'sea bass', 'sea bass', 'olive flounder', 'dace', 'shark'])
+	myocean.fishin()
+	a4 = raw_input('')
+	print('my pocket contains: ')
+	my_pocket.list()
+	print('')
+else: pass		
+
+#walk = raw_input('do you want to walk around yor island? ')
+
---
+++ shapemodule.py
@@ -0,0 +1,167 @@
+class Canvas:
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+		self.data = [['  '] * width for i in range(height)]
+
+	def setpixel(self, brush, row, col):
+		self.data[row][col] = brush
+
+	def getpixel(self, row, col):
+		return self.data[row][col]
+
+	def display(self):
+		print('\n'.join([''.join(row) for row in self.data]))
+
+	def clear(self):
+		self.data = [['  '] * self.width for i in range(self.height)]
+
+class Shape:
+	def paint(self, canvas): pass
+
+class Rectangle(Shape):
+	def __init__(self, x, y, w, h):
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+
+	def paint(self, canvas, brush):
+		for y in range(self.h):
+			for x in range(self.w):
+				canvas.setpixel(brush, (self.y + y), (self.x + x))
+
+class Circle(Shape):
+	def __init__(self, radius, x, y):
+		self.radius = radius
+		self.x = x
+		self.y = y
+		self.diameter = (self.radius * 2) - 1
+		if (self.radius % 2) == 1:
+			self.startwidth = self.radius
+		else:
+			self.startwidth = (self.radius - 1)
+		self.edgex = self.x - (self.radius - 1)
+		self.edgey = self.y - (self.radius - 1)
+
+	def paint(self, canvas, brush):
+		for y in range((self.diameter - self.startwidth) / 2):
+			numpixels = self.startwidth + (y * 2)
+			for x in range(numpixels):
+				canvas.setpixel(brush, (self.edgey + y), (self.edgex + ((self.diameter - self.startwidth) / 2) - y + x))
+
+		for y in range(self.startwidth):
+			for x in range(self.diameter):
+				canvas.setpixel(brush, (self.edgey + ((self.diameter - self.startwidth) / 2) + y), (self.edgex + x))
+
+		for y in range((self.diameter - self.startwidth) / 2):
+			if y == 0: numpixels -= 0
+			else: numpixels -= 2
+			for x in range(numpixels):
+				canvas.setpixel(brush, (self.edgey + self.diameter - ((self.diameter - self.startwidth) / 2) + y), ((self.edgex + (y + 1)) + x))
+
+class Triangle(Shape):
+	def __init__(self, height, angle, x, y):
+		self.height = height
+		self.angle = angle
+		self.x = x
+		self.y = y
+
+	def paint(self, canvas, brush):
+		if self.angle == 45:
+			w = 1
+			for y in range(self.height):
+				for x in range(w):
+					canvas.setpixel(brush, (self.y + y), ((self.x - y) + x))
+				w += 2
+
+		elif self.angle == 60:
+			w = 1
+			for y in range(self.height):
+				for x in range(w):
+					canvas.setpixel(brush, (self.y + y), ((self.x - (w // 2)) + x))
+				if (y % 2) == 1: w += 2
+				else: pass
+
+		elif self.angle == 75:
+			w = 1
+			for y in range(self.height):
+				for x in range(w):
+					canvas.setpixel(brush, (self.y + y), ((self.x - (w // 2)) + x))
+				if (y % 3) == 2: w += 2
+				else: pass
+	
+class RightTri(Triangle):
+	def __init__(self, height, angle, x, y, orient):
+		self.height = height
+		self.angle = angle
+		self.x = x
+		self.y = y
+		self.orient = orient
+
+	def paint(self, canvas, brush):
+		if self.orient == 'RU':
+			w = 1
+			for y in range(self.height):
+				for x in range(w):
+					canvas.setpixel(brush, (self.y + y), (self.x + x))
+				if self.angle == 45:
+					w += 1
+				elif self.angle == 60:
+					if (y % 2) == 1: w += 1
+					else: pass
+				elif self.angle == 75:
+					if (y % 3) == 2: w += 1
+					else: pass
+
+		if self.orient == 'LU':
+			w = 1
+			for y in range(self.height):
+				for x in range(w):
+					canvas.setpixel(brush, (self.y + y), ((self.x - y) + x))
+				if self.angle == 45:
+					w += 1
+				elif self.angle == 60:
+					if (y % 2) == 1: w += 1
+					else: pass
+				elif self.angle == 75:
+					if (y % 3) == 2: w += 1
+					else: pass
+
+		if self.orient == 'RD':
+			if self.angle == 45:
+				w = this_triangle.height
+			elif self.angle == 60:
+				w = (this_triangle.height // 2) + 1
+			elif self.angle == 75:
+				pass
+			for y in range(self.height):
+				for x in range(w):
+					canvas.setpixel(brush, (self.y + y), (self.x + x))
+				if self.angle == 45:
+					w += 1
+				elif self.angle == 60:
+					if (y % 2) == 1: w += 1
+					else: pass
+				elif self.angle == 75:
+					if (y % 3) == 2: w += 1
+					else: pass
+
+		if self.orient == 'LD':
+			w = 1
+			for y in range(self.height):
+				for x in range(w):
+					canvas.setpixel(brush, (self.y + y), (self.x + x))
+				if self.angle == 45:
+					w += 1
+				elif self.angle == 60:
+					if (y % 2) == 1: w += 1
+					else: pass
+				elif self.angle == 75:
+					if (y % 3) == 2: w += 1
+					else: pass
+
+
+def hi():
+	print('hi turtles')
+
